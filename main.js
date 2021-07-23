4f99c23ca88cdd99255df0936bff56de8ef5a974
Aarav-is-EPIC <76231563+Aarav-is-EPIC@users.noreply.github.com>
2021-07-23 10:58:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup() {
+  canvas = createCanvas(300,300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier= ml5.imageClassifier('MobileNet',modelLoaded);
+
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
